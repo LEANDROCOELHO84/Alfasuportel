c8bf73c1964f5b43806f9d3e9977613ccec702d5
rafaelqw <rafaoliveirabispo@gmail.com>
2018-03-16 09:36:50 -0300

    melhorias menu e identação

---
+++ js/angular-services/user-service.js
@@ -0,0 +1,23 @@
+var app = angular.module('alfasuportel', []);
+app.controller('indexController', function($scope, $http){
+
+	$scope.loadCategorias = function() {
+		$scope.categorias = [];
+		$scope.subcategorias = [];
+
+		$http.get(baseUrlApi()+"categorias/treeview?tce->id_empreendimento=217").then(function(response){
+			$scope.categorias = response.data;
+		}, function(err){
+			console.log(err);
+		});
+	};
+
+	$scope.loadSubCategorias = function(item){
+		$scope.subcategorias = [];
+		if (item.filhos != null) {
+			$scope.subcategorias = item.filhos
+		}
+	};
+
+	$scope.loadCategorias();
+});
