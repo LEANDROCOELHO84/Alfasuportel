0cebcae5a21894e09c4291750973fc0f14bfa995
leandro <leandromendoncacoelho@gmail.com>
2018-06-03 03:35:24 -0300

    ajustes

--- js/angular-controller/subcategoria-controller.js
+++ js/angular-controller/subcategoria-controller.js
@@ -46,15 +46,15 @@ app.controller('subcategoriaController', function($scope, $http){
 	$scope.loadSubcategoriasByCategorias = function() {
 		$scope.itens = [];
 
-		$http.get(baseUrlApi()+"categorias/treeview?tce->id_empreendimento=217").then(function(response){
-			angular.forEach(response.data, function(item, index){
-				if(item.id == parseInt(params.categoria,10)){
-					$scope.itens = item.filhos;
+		$http.get(baseUrlApi()+"categorias?tce->id_empreendimento=217&id_pai="+ params.categoria +"&recursively=1")
+			.then(
+				function(response){
+					$scope.itens = response.data.categorias;
 					angular.forEach($scope.itens, function(x,y){
 						if(x.pth_thumbnail != null)
 							x.pth_thumbnail = x.pth_thumbnail.substring(x.pth_thumbnail.indexOf('assets'), x.pth_thumbnail.length);
 						else
-							x.pth_thumbnail = '/path/to/sample/image.png';
+						x.pth_thumbnail = '/path/to/sample/image.png';
 
 						if(x.pth_banner != null)
 							x.pth_banner = x.pth_banner.substring(x.pth_banner.indexOf('assets'), x.pth_banner.length);
@@ -74,18 +74,17 @@ app.controller('subcategoriaController', function($scope, $http){
 							angular.forEach(x.filhos, function(a,b){
 								if(a.pth_thumbnail != null)
 									a.pth_thumbnail = a.pth_thumbnail.substring(a.pth_thumbnail.indexOf('assets'), a.pth_thumbnail.length);
-
 								else
 									a.pth_thumbnail = '/path/to/sample/image.png';
 							});
 						}
 					});
+				}, function(err){
+					console.log(err);
 				}
-			});
-		}, function(err){
-			console.log(err);
-		});
-	};
+			);
+
+	}
 
 	// Banner
 
